ac920c7086ef0e201826d712ca1ddb23bb427eae
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 10:08:08 +0300

    feat: add two-way binding component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OneWayBindingComponent } from "./one-way-binding/one-way-binding.component";
+import { TwoWayBindingComponent } from "./two-way-binding/two-way-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OneWayBindingComponent],
+  imports: [RouterOutlet, OneWayBindingComponent, TwoWayBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

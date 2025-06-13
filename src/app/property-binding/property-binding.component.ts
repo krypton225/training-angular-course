5227a56b1e4676fb5bd2302efdcb6ebb9af8cf41
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 14:53:48 +0300

    feat: add PropertyBindingComponent with template, styles, and tests

---
+++ src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  imports: [],
+  templateUrl: './property-binding.component.html',
+  styleUrl: './property-binding.component.scss'
+})
+export class PropertyBindingComponent {
+  propExample: string = `https://i.pinimg.com/736x/ef/b5/cd/efb5cd5b289ef77b44703fd0fab69a71.jpg`;
+}

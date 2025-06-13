38187a70b52b2015042a58968acedf6345748884
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 14:52:56 +0300

    feat: add AttributeBindingComponent with template, styles, and tests

---
+++ src/app/attribute-binding/attribute-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-binding',
+  imports: [],
+  templateUrl: './attribute-binding.component.html',
+  styleUrl: './attribute-binding.component.scss'
+})
+export class AttributeBindingComponent {
+  isCollapsed: boolean = true;
+}

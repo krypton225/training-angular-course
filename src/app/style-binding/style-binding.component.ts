6211b36781961bc2b194869c77829622c83b95bc
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 14:53:53 +0300

    feat: add StyleBindingComponent with template, styles, and tests

---
+++ src/app/style-binding/style-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style-binding',
+  imports: [],
+  templateUrl: './style-binding.component.html',
+  styleUrl: './style-binding.component.scss'
+})
+export class StyleBindingComponent {
+
+}

7ad427e47e5bacda591572bf8b53d93722278fc8
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 14:53:32 +0300

    feat: refactor OneBindingExampleComponent to use router links & simplify template

--- src/app/one-binding-example/one-binding-example.component.ts
+++ src/app/one-binding-example/one-binding-example.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-one-binding-example',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './one-binding-example.component.html',
   styleUrl: './one-binding-example.component.scss'
 })
 export class OneBindingExampleComponent {
-  interpolationExample: string = "Just an example for interpolation";
-  hasBgRed: boolean = false;
 
-  hasBgBlue: boolean = true;
-  hasPaddingBlock: boolean = true;
-  hasPaddingInline: boolean = false;
-
-  isCollapsed: boolean = true;
-
-  propExample: string = `https://i.pinimg.com/736x/ef/b5/cd/efb5cd5b289ef77b44703fd0fab69a71.jpg`;
-
-  buttonHandler(): void {
-    this.hasBgRed = !this.hasBgRed;
-  }
 }

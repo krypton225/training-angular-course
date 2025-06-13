b3599e297551ae0b7c69a814b7074efca1e1393b
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 10:33:20 +0300

    feat: add ControlFlowComponent to app & update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OneWayBindingComponent } from "./one-way-binding/one-way-binding.component";
 import { TwoWayBindingComponent } from "./two-way-binding/two-way-binding.component";
+import { ControlFlowComponent } from "./control-flow/control-flow.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OneWayBindingComponent, TwoWayBindingComponent],
+  imports: [
+    RouterOutlet,
+    OneWayBindingComponent,
+    TwoWayBindingComponent,
+    ControlFlowComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

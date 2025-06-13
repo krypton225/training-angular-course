91a90d8bdda9a8f775c5fc135916943fdcdb9625
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 09:37:53 +0300

    feat: implement prop binding in `one-way-binding` component

--- src/app/one-way-binding/one-way-binding.component.ts
+++ src/app/one-way-binding/one-way-binding.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class OneWayBindingComponent {
   interpolationExample: string = "Just an example for interpolation";
+
+  propExample: string = `https://i.pinimg.com/736x/ef/b5/cd/efb5cd5b289ef77b44703fd0fab69a71.jpg`;
 }

31d6081349c905c20870293eeea70a4d4e4db092
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 09:36:28 +0300

    feat: implement interpolation binding in `one-way-binding` component

---
+++ src/app/one-way-binding/one-way-binding.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { OneWayBindingComponent } from './one-way-binding.component';
+
+describe('OneWayBindingComponent', () => {
+  let component: OneWayBindingComponent;
+  let fixture: ComponentFixture<OneWayBindingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OneWayBindingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OneWayBindingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/one-way-binding/one-way-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-way-binding',
+  templateUrl: './one-way-binding.component.html',
+  styleUrls: ['./one-way-binding.component.scss']
+})
+export class OneWayBindingComponent {
+  interpolationExample: string = "Just an example for interpolation";
+}

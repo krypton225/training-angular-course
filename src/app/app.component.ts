4f28f2ec9fb3d916f70a20080cde47c2f71a3e4b
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-13 09:36:41 +0300

    feat: add OneWayBindingComponent to app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OneWayBindingComponent } from "./one-way-binding/one-way-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OneWayBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

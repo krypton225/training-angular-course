9697294023a0ca17094e01cf02ab3bc803c32cac
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-14 14:28:42 +0300

    feat: enhance ViewChildExampleComponent with API call & lifecycle methods

--- src/app/view-child-example/view-child-example.component.ts
+++ src/app/view-child-example/view-child-example.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-view-child-example',
@@ -6,10 +6,25 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './view-child-example.component.html',
   styleUrl: './view-child-example.component.scss'
 })
-export class ViewChildExampleComponent {
+export class ViewChildExampleComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
   @ViewChild("paragraphElement") paragraphElement !: ElementRef;
 
   changeText(): void {
     this.paragraphElement.nativeElement.textContent = `The text was changed!`;
+  }
+
+  async getTodos(): Promise<void> {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  ngOnDestroy(): void {
+    console.log("You moved to another routing!");
   }
 }

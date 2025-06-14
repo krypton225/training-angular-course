a7d01eb910abdbeda112ee1f95d86431242a3a65
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-14 14:35:50 +0300

    feat: implement OnChanges lifecycle hook in AtInputChildComponent

--- src/app/at-input-child/at-input-child.component.ts
+++ src/app/at-input-child/at-input-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-at-input-child',
@@ -6,6 +6,10 @@ import { Component, Input } from '@angular/core';
   templateUrl: './at-input-child.component.html',
   styleUrl: './at-input-child.component.scss'
 })
-export class AtInputChildComponent {
+export class AtInputChildComponent implements OnChanges {
   @Input({ required: true, }) receivedPhoneNumber: string = ``;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }

b26c17c8d4efbde0955bdcaddb2ca6ef2c98db08
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-14 09:55:35 +0300

    feat: enhance content projection example with button click handler & span reference

--- src/app/content-projection-example/content-projection-example.component.ts
+++ src/app/content-projection-example/content-projection-example.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-content-projection-example',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './content-projection-example.component.scss'
 })
 export class ContentProjectionExampleComponent {
+  @ContentChild("spanEl") spanEl !: ElementRef<HTMLSpanElement>;
 
+  showTextOfSpanHandler(): void {
+    console.log(this.spanEl.nativeElement.textContent);
+  }
 }

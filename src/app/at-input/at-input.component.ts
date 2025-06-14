af63ab4b410e534b6e6561117e51825fa5c740a7
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-14 10:29:05 +0300

    feat: create AtInputComponent with template, styles, and unit tests

---
+++ src/app/at-input/at-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AtInputParentComponent } from "../at-input-parent/at-input-parent.component";
+
+@Component({
+  selector: 'app-at-input',
+  imports: [AtInputParentComponent],
+  templateUrl: './at-input.component.html',
+  styleUrl: './at-input.component.scss'
+})
+export class AtInputComponent {
+
+}
